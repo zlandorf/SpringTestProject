d6491f3c795167ebbd5b8714357cdf5dc8bfd8ef
zlandorf <pmilian33@gmail.com>
2015-04-18 22:03:47 +0200

    Adding initial source code for a SpringPrototype project

---
+++ src/main/java/demo/error/JSPObject.java
@@ -0,0 +1,13 @@
+package demo.error;
+
+public class JSPObject {
+	private String label;
+	
+	public JSPObject(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	}
+}
