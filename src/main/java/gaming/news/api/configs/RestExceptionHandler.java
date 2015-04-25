0c0311633f4c8c853f18aa883676d65ff1650af1
zlandorf <pmilian33@gmail.com>
2015-04-25 16:22:34 +0200

    Fixed exception handling

--- src/main/java/gaming/news/api/configs/RestExceptionHandler.java
+++ src/main/java/gaming/news/api/configs/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import gaming.news.api.exceptions.InvalidParameterException;
 import gaming.news.api.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +14,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public void resourceNotFoundException(RuntimeException e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void resourceNotFoundException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidParameterException.class)
+    public void invalidParameterException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 }
